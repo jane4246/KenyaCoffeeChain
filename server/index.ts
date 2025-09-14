cb6dac05c4d22551fdb9de1925ebec3ed5200a57
maritimjane <58557449+jane4246@users.noreply.github.com>
2025-09-14 14:04:14 -0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
+console.log("DATABASE_URL =>", process.env.DATABASE_URL);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
